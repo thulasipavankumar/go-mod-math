553afc669f8224f34ff36550e0eed7addb11ca79
thulasipavankumar <thulasipavankumar@gmail.com>
2022-12-21 19:06:45 +0530

    added new feature for checking +ve integer in function

--- arithmetic/arithmetic.go
+++ arithmetic/arithmetic.go
@@ -1,4 +1,5 @@
 package arithmetic
+import "errors"
 func IsPrime(num int) bool {
     for i := 2; i < int(num/2); i++ {
         if num%i == 0 {
@@ -8,10 +9,13 @@ func IsPrime(num int) bool {
     return true
 }
 
-func Factorial(n int) int {
+func Factorial(n int) (int,error) {
+    if n<0  {
+     return 0,errors.New("Please provide a +ve number greater than zero")
+    }
     var f int = 1
     for i := 2; i <= n; i++ {
         f *= i
     }
-    return f
+    return f,nil
 }
