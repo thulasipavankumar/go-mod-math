5ae2f7b5f5f731ef78cad542912ad7a1ba9c9332
ambatisandeep <sandeepreddy.ambati@gmail.com>
2022-12-21 18:35:24 +0530

    added files to the package

---
+++ arithmetic.go
@@ -0,0 +1,17 @@
+package arithmetic
+func IsPrime(num int) bool {
+    for i := 2; i < int(num/2); i++ {
+        if num%i == 0 {
+            return false
+        }
+    }
+    return true
+}
+
+func Factorial(n int) int {
+    var f int = 1
+    for i := 2; i <= n; i++ {
+        f *= i
+    }
+    return f
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/thulasipavankumar/go-mod-math
+
+go 1.19
